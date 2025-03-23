cd6533b763516257e9a82b428b103d8f480ce714
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-23 13:54:40 +0100

    text

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,30 @@
 const Navbar = () => {
+  const buttonArticle={
+    backgroundColor:'blue',
+    color:'white',
+    padding:'10px',
+    borderRadius:'5px',
+    cursor:'pointer',
+
+  }
   return (
     <nav className="navbar">
       <div>
-        <a href="">Boris dev</a>
+        <a href="" className="logo">Boris dev</a>
       </div>
       <ul className="liens">
         <li>
           <a href="" className="lien">Accueil</a>
         </li>
         <li>
-          <a href="" className="lien">Creer un article</a>
+          <a href="" className="lien" style={ buttonArticle
+          //   {backgroundColor:'blue',
+          // color:'white',
+          // padding:'10px',
+          // borderRadius:'5px'
+
+          // }
+          }>Creer un article</a>
         </li>
         
       </ul>
