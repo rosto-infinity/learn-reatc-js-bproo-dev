c8fc0f611b04b5ace7d4214337067eb0cb1c9fb7
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-25 17:06:01 +0100

    text

---
+++ src/Add.jsx
@@ -0,0 +1,9 @@
+const Add = () => {
+    return (
+        <div className="create">
+            <h2>Ajouter un nouvel article</h2>
+        </div>
+      );
+}
+ 
+export default Add;
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Add from "./Add";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
@@ -48,14 +49,14 @@ function App() {
 
   return (
     <Router>
-    <div className="App">
+      <div className="App">
         <Navbar />
         <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Ajoutez d'autres routes ici */}
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Add/>} />
         </Routes>
-    </div>
-</Router>
+      </div>
+    </Router>
 
   )
 }
